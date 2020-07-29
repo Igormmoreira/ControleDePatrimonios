3f237b2bf6b600e2af27a7cc8028778a4b2b072e
Igor Moreira <immoreira94@gmail.com>
2020-07-29 19:04:59 -0300

    Add models Patrimonio e Marca

---
+++ ControleDePatrimonios/ControleDePatrimonios/Models/Marca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDePatrimonios.Models
+{
+    public class Marca
+    {
+        public string Nome { get; set; }
+        public int MarcaId { get; set; }
+    }
+}
---
+++ ControleDePatrimonios/ControleDePatrimonios/Models/Patrimonio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDePatrimonios.Models
+{
+    public class Patrimonio
+    {
+        public string Nome { get; set; }
+        public int MarcaID { get; set; }
+        public string Descricao { get; set; }
+        public int NumeroTombo { get; set; }
+    }
+}
