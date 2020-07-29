c000fae51e29b6323901953f3c06feed33740d5e
Igor Moreira <immoreira94@gmail.com>
2020-07-29 18:54:34 -0300

    Project created

---
+++ ControleDePatrimonios/ControleDePatrimonios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControleDePatrimonios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
